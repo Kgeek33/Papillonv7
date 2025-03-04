cb0179a69d62d397d48ba1c9bd3e3697fae3d0b2
Kgeek33 <kylian.jarry@scaramouch.com>
2025-03-04 14:57:47 +0100

    suppression du checkout + indentation

--- modifyConfig.js
+++ modifyConfig.js
@@ -4,11 +4,14 @@ const path = require("path");
 const configPath = path.join(__dirname, "app.config.ts");
 let configContent = fs.readFileSync(configPath, "utf8");
 
-// Remplace "Papillon" par "Papillon Dev"
-configContent = configContent.replace(/name: "Papillon"/, "name: \"Papillon Dev\"");
-
-// Remplace "xyz.getpapillon.app" par "xyz.getpapillon.app.dev"
-configContent = configContent.replace(/package: "xyz.getpapillon.app"/, "package: \"xyz.getpapillon.app.dev\"");
+configContent = configContent.replace(
+  /name: "Papillon"/,
+  "name: \"Papillon Dev\""
+);
+configContent = configContent.replace(
+  /package: "xyz.getpapillon.app"/,
+  "package: \"xyz.getpapillon.app.dev\""
+);
 
 fs.writeFileSync(configPath, configContent, "utf8");
 console.log("app.config.ts modifié avec succès");
